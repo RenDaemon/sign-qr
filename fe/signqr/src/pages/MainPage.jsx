487c1d93bf9b94f0211b92290570c0a22a87c393
sedtia-prakoso-budi-tirto-arto <99130485+sedtia-prakoso-budi-tirto-arto@users.noreply.github.com>
2023-06-15 11:15:19 +0700

    Update MainPage.jsx

--- fe/signqr/src/pages/MainPage.jsx
+++ fe/signqr/src/pages/MainPage.jsx
@@ -6,7 +6,7 @@ export default function MainPage() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-iwhite px-10 py-16">
       <div className="flex w-full pb-10 lg:pb-16">
-        <span className="mx-auto text-4xl font-extrabold text-ipurple md:text-5xl lg:text-6xl">
+        <span className="mx-auto text-4xl font-extrabold text-iyellow md:text-5xl lg:text-6xl">
           Sign-Qr
         </span>
       </div>
